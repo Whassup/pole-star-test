887171571fd35fb1e047cef0b2250788a6f95f01
Robert Prib <rprib@funnelback.com>
2017-08-10 10:19:27 +0100

    Generated new component using 'ng g c' for screening-profile-table

---
+++ src/app/screening-profile-table/screening-profile-table.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScreeningProfileTableComponent } from './screening-profile-table.component';
+
+describe('ScreeningProfileTableComponent', () => {
+  let component: ScreeningProfileTableComponent;
+  let fixture: ComponentFixture<ScreeningProfileTableComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ScreeningProfileTableComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ScreeningProfileTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/screening-profile-table/screening-profile-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screening-profile-table',
+  templateUrl: './screening-profile-table.component.html',
+  styleUrls: ['./screening-profile-table.component.css']
+})
+export class ScreeningProfileTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
