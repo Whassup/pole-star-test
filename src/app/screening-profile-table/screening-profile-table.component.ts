f7b47c8879c60c3407d761c937cd994ac919c33a
Robert Prib <rprib@funnelback.com>
2017-08-10 12:38:45 +0100

    Added basic listing of profiles in table

--- src/app/screening-profile-table/screening-profile-table.component.ts
+++ src/app/screening-profile-table/screening-profile-table.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./screening-profile-table.component.css']
 })
 export class ScreeningProfileTableComponent implements OnInit {
+  profiles:Array<Object> = [{
+    "id": "eaad94d1-4e5c-496b-a565-50bf4a6d1262",
+    "created": "2016-03-07T08:01:46.013000Z",
+    "modified": "2016-03-07T08:01:46.014000Z",
+    "name": "Default Screening Profile",
+    "country_check_severity": "90-CRITICAL"
+  }]
 
   constructor() { }
 
