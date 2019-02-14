1033bf6d4f73b68b5b2bc9a8b775347f1400ab83
Robert Prib <robert.prib@gmail.com>
2019-02-14 16:37:02 +1100

    Fix unit test and e2e test failures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { ScreeningProfilesService } from './screening-profiles.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ScreeningProfileTableComponent } from './screening-profile-table/screening-profile-table.component';
+import { ScreeningProfilesService } from './screening-profiles.service';
 
 @NgModule({
   declarations: [
--- src/app/screening-profile-table/screening-profile-table.component.spec.ts
+++ src/app/screening-profile-table/screening-profile-table.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { ScreeningProfileTableComponent } from './screening-profile-table.component';
+import { ScreeningProfilesService } from '../screening-profiles.service';
 
 describe('ScreeningProfileTableComponent', () => {
   let component: ScreeningProfileTableComponent;
@@ -8,7 +12,13 @@ describe('ScreeningProfileTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScreeningProfileTableComponent ]
+      imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule
+      ],
+      declarations: [ ScreeningProfileTableComponent ],
+      providers: [ScreeningProfilesService]
     })
     .compileComponents();
   }));
